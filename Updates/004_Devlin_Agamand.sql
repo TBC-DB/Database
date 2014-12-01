42b65b221531f55b04582dba9af762cfd680acd4
Christian Ruppert <idl0r@qasl.de>
2014-12-02 00:07:06 +0100

    Devlin Agamand was way too strong
    Use the ClassicDB values
    http://www.wowhead.com/npc=1657

---
+++ Updates/004_Devlin_Agamand.sql
@@ -0,0 +1,3 @@
+-- He was way too strong. Use the ClassicDB values.
+-- http://www.wowhead.com/npc=1657
+UPDATE `creature_template` SET `MinMeleeDmg`=20, `MaxMeleeDmg`=32, `MeleeAttackPower`=12 WHERE `entry`=1657;
