d425ba4e017db5091aea2ba3e0f8c176ea2859ea
Tobschinski <tobschinski@gmail.com>
2016-03-12 19:35:22 +0100

    Terrokar - Terokkarantula & Blood Furnace - Fel Orc Neophyte & Nascent Fel Orc
    + assigned (the correct) model to Terokkarantula
    + removed an alternative model for Fel Orc Neophyte & Nascent Fel Orc
    
    ------
    
    https://report.nostalrius.org/plugins/tracker/?aid=4195
    https://report.nostalrius.org/plugins/tracker/?aid=4354

---
+++ Updates/Custom_Data/0003_01_creature_template.sql
@@ -0,0 +1,3 @@
+
+-- Terokkarantula
+UPDATE `creature_template` SET `ModelId1`='19716', `ModelId3`='0' WHERE `Entry`='20682';
---
+++ Updates/Custom_Data/0003_02_creature_model_info.sql
@@ -0,0 +1,3 @@
+
+-- Fel Orc Neophyte & Nascent Fel Orc
+UPDATE `creature_model_info` SET `modelid_other_gender`='0' WHERE `modelid`='16332';
