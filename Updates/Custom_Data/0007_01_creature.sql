9606ace893170a74c9d7439df1b1b06dbfee145e
Tobschinski <tobschinski@gmail.com>
2016-03-14 17:30:38 +0100

    Hellfire Peninsula - Mistress of Doom
    + removed double spawns
    + added waypoints

---
+++ Updates/Custom_Data/0007_01_creature.sql
@@ -0,0 +1,7 @@
+
+-- Mistress of Doom
+DELETE FROM `creature` WHERE `guid` IN ('68722', '68723', '68724');
+
+UPDATE `creature` SET `position_x`='408.4745', `position_y`='3264.019', `position_z`='74.64674', `orientation`='1.301382', `spawndist`='0', `MovementType`='2' WHERE `guid`='68725';
+UPDATE `creature` SET `position_x`='384.3363', `position_y`='3257.237', `position_z`='74.62700', `orientation`='2.503842', `spawndist`='0', `MovementType`='2' WHERE `guid`='68726';
+UPDATE `creature` SET `position_x`='329.0792', `position_y`='3319.538', `position_z`='71.74088', `orientation`='1.021431', `spawndist`='0', `MovementType`='2' WHERE `guid`='68727';
