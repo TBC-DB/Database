cf928eca017c81f7a600c8b29e9cea32a025af17
Tobschinski <tobschinski@gmail.com>
2016-03-20 17:05:18 +0100

    Netherstorm - Mageslayer, Mana Seeker & Mana Beast
    + Mageslayer, Mana Seeker & Mana Beast do arcane damage, not phsyical

---
+++ Updates/Custom_Data/0013_01_creature_template.sql
@@ -0,0 +1,9 @@
+
+-- Mageslayer
+UPDATE `creature_template` SET `DamageSchool`='6' WHERE `Entry`='18866';
+
+-- Mana Seeker
+UPDATE `creature_template` SET `DamageSchool`='6' WHERE `Entry`='18867';
+
+-- Mana Beast
+UPDATE `creature_template` SET `DamageSchool`='6' WHERE `Entry`='21267';
