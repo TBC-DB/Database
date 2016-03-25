1d142d999ac095de0bfbcdbe1cb6c254678d6d7c
Tobschinski <tobschinski@gmail.com>
2016-03-25 03:19:01 +0100

    Blood Furnace

---
+++ Updates/Custom_Data/0014_03_creature_template.sql
@@ -0,0 +1,38 @@
+
+-- ---| Equipment |---
+
+UPDATE `creature_template` SET `EquipmentTemplateId`='5114' WHERE `Entry`='17626';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5593' WHERE `Entry`='17491';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5586' WHERE `Entry`='17624';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5481' WHERE `Entry`='17414';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5097' WHERE `Entry`='18894';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5097' WHERE `Entry`='17400';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5594' WHERE `Entry`='17398';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5595' WHERE `Entry`='17377';
+UPDATE `creature_template` SET `EquipmentTemplateId`='5596' WHERE `Entry`='17256';
+
+-- ---| Model & Faction |---
+
+UPDATE `creature_template` SET `ModelId2`='9609', `ModelId3`='0' WHERE `Entry`='17256';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17370';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17371';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17377';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17380';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17381';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17395';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17397';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17398';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17399';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='90', `FactionHorde`='90' WHERE `Entry`='17400';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17401';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17414';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='1652', `FactionHorde`='1652' WHERE `Entry`='17416';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17429';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17491';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17624';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='17626';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='17653';
+UPDATE `creature_template` SET `ModelId3`='0', `FactionAlliance`='90', `FactionHorde`='90' WHERE `Entry`='18894';
+UPDATE `creature_template` SET `ModelId2`='16891', `ModelId3`='0', `FactionAlliance`='16', `FactionHorde`='16' WHERE `Entry`='19016';
+UPDATE `creature_template` SET `ModelId3`='0' WHERE `Entry`='21174';
+UPDATE `creature_template` SET `ModelId2`='16891', `ModelId3`='0' WHERE `Entry`='17477';
