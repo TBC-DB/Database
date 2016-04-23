118fca21fcc3bb8dc74d5f480008dd8cc5150353
cherbert <chris@herbert.net>
2016-04-23 13:46:21 +0100

    Update quest.9280.9369.9371.sql
    Corrected field Entry to lowercase

--- Updates/quest.9280.9369.9371.sql
+++ Updates/quest.9280.9369.9371.sql
@@ -1,4 +1,4 @@
 -- q.9280 q.9369 'Replenishing the Healing Crystals'
-UPDATE `quest_template` SET NextQuestId = 9409 WHERE Entry = 9280 AND Entry = 9369;
+UPDATE `quest_template` SET NextQuestId = 9409 WHERE entry = 9280 AND entry = 9369;
 -- q.9371 'Botanist Taerix'
-UPDATE `quest_template` SET PrevQuestId = 9409 WHERE Entry = 9371;
+UPDATE `quest_template` SET PrevQuestId = 9409 WHERE entry = 9371;
