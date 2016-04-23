a75497ebfb45d0c5f54f435f90429fb1618dcb89
Saben65 <ebecker10@gmail.com>
2016-04-23 17:32:44 -0600

    Adds correct modelIDs to all NPCs in karazhan.

---
+++ Updates/Custom_Data/2016_04_23_Creature_Template.sql
@@ -0,0 +1,30 @@
+-- Spectral Servant
+UPDATE `creature_template` SET `modelid2`=16465, `modelid3`=16466, `modelid4`=16467 WHERE `entry`=16409;
+-- Phantom Guest
+UPDATE `creature_template` SET `modelid2`=16486, `modelid3`=16487, `modelid4`=16488 WHERE `entry`=16407;
+-- Ghostly Steward
+UPDATE `creature_template` SET `modelid2`=16536, `modelid3`=16537, `modelid4`=16538 WHERE `entry`=16414;
+-- Spectral Chef
+UPDATE `creature_template` SET `modelid2`=16525, `modelid3`=16526, `modelid4`=16527 WHERE `entry`=16411;
+-- Ghostly Baker
+UPDATE `creature_template` SET `modelid2`=16530, `modelid3`=16531, `modelid4`=16532 WHERE `entry`=16412;
+-- Phantom Valet
+UPDATE `creature_template` SET `modelid2`=16495, `modelid3`=16496, `modelid4`=16497 WHERE `entry`=16408;
+-- Spectral Sentry
+UPDATE `creature_template` SET `modelid2`=16459, `modelid3`=16460, `modelid4`=16461 WHERE `entry`=16424;
+-- Wanton Hostess
+UPDATE `creature_template` SET `modelid2`=16544, `modelid3`=16545, `modelid4`=16546 WHERE `entry`=16459;
+-- Concubine
+UPDATE `creature_template` SET `modelid2`=16552, `modelid3`=16553, `modelid4`=16554 WHERE `entry`=16461;
+-- Night Mistress
+UPDATE `creature_template` SET `modelid2`=16548, `modelid3`=16549, `modelid4`=16550 WHERE `entry`=16460;
+-- Spectral Patron
+UPDATE `creature_template` SET `modelid2`=16556, `modelid3`=16557, `modelid4`=16558 WHERE `entry`=16468;
+-- Spectral Performer
+UPDATE `creature_template` SET `modelid2`=16568, `modelid3`=16569, `modelid4`=18979 WHERE `entry`=16473;
+-- Phantom Stagehand
+UPDATE `creature_template` SET `modelid2`=16564, `modelid3`=16565, `modelid4`=16566 WHERE `entry`=16472;
+-- Ghostly Philanthropist
+UPDATE `creature_template` SET `modelid2`=16560, `modelid3`=16561, `modelid4`=16562 WHERE `entry`=16470;
+-- Shadow Pillager
+UPDATE `creature_template` SET `modelid2`=16905, `modelid3`=16906, `modelid4`=16907 WHERE `entry`=16540;
