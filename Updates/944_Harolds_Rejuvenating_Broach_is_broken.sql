7d01291c052fe852a06cce7f5e6983a71aa17a3f
Leadballoon2000 <Leadballoon@liamday.net>
2016-05-30 23:55:52 +0100

    #944 Harolds Rejuvenating Broach is broken

---
+++ Updates/944_Harolds_Rejuvenating_Broach_is_broken.sql
@@ -0,0 +1,3 @@
+-- Links the aura from Harold's Rejuvenating Broach to Rejevenation spell (spellmask 0x10)
+  INSERT INTO `spell_affect` (`entry`, `effectID`, `SpellFamilyMask`) VALUES (32402, 0, 16);
+  
