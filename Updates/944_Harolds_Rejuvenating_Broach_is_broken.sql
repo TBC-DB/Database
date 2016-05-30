824b55296cc3faea3d13b64fdaef76c40fecfa77
Leadballoon2000 <Leadballoon@liamday.net>
2016-05-31 00:14:28 +0100

    #947 Idol of Rejuvenation is broken

--- Updates/944_Harolds_Rejuvenating_Broach_is_broken.sql
+++
@@ -1,3 +0,0 @@
--- Links the aura from Harold's Rejuvenating Broach to Rejevenation spell (spellmask 0x10)
-  INSERT INTO `spell_affect` (`entry`, `effectID`, `SpellFamilyMask`) VALUES (32402, 0, 16);
-  
