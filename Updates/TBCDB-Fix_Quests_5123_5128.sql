91a17eaa42580864d2a7a4f1ea7b08be576dcf42
Shawn Diviney <diviney@rdacorp.com>
2016-08-06 17:03:22 -0400

    reconcile with UDB values

--- Updates/TBCDB-Fix_Quests_5123_5128.sql
+++ Updates/TBCDB-Fix_Quests_5123_5128.sql
@@ -1,3 +1,3 @@
 -- Fix quest chain 5128 (Words of the High Chief)
 
-UPDATE `mangos`.`conditions` SET `value1` = 5087 WHERE `condition_entry` = 211;
+UPDATE `mangos`.`conditions` SET `value1` = 5121, `type` = 9 WHERE `condition_entry` = 211;
